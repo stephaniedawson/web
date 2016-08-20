bc7a9739bdfc2de6bae220c33c3ef2bf730a8507
Stephanie Dawson <stephanie.a.dawson@gmail.com>
2016-08-20 18:19:31 -0500

    adding js and php

--- scripts.js
+++ scripts.js
@@ -21,3 +21,31 @@ $(document).ready(function() {
                 $('#carousel-text').html($('#slide-content-'+id).html());
         });
 });
+
+$("#contactForm").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var subject =$("#subject").val();
+    var message = $("#message").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "process.php",
+        data: "name=" + name + "&email=" + email + "&subject=" +subject + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+function formSuccess(){
+    $( "#msgSubmit" ).removeClass( "hidden" );
+}
